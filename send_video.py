dc5a8c422b61471a7d4bfc6db2644ebac1051607
Rick Giuly <rgiuly@gmail.com>
2016-12-26 06:17:52 +0000

    commit

--- send_video.py
+++ send_video.py
@@ -108,10 +108,10 @@ def handleLinux(deviceNumber, videoPort):
     #print err
 
     #os.system("v4l2-ctl -c brightness=10 -c contrast=25 -c saturation=40")
-    os.system("v4l2-ctl -c brightness=240 -c contrast=75 -c saturation=60") # Skippy   
+    #os.system("v4l2-ctl -c brightness=240 -c contrast=75 -c saturation=60") # Skippy
     #os.system("v4l2-ctl -c brightness=50 -c contrast=50 -c saturation=80")
     #os.system("v4l2-ctl -c brightness=200 -c contrast=100 -c saturation=100")
-    
+    os.system("v4l2-ctl -c brightness=20 -c contrast=50 -c saturation=60") # Jenny
 
     if deviceNumber is None:
         deviceAnswer = raw_input("Enter the number of the camera device for your robot: ")
