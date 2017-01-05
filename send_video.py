63c2b9d43d92a43898d327dcec9edf26f76058e3
Rick Giuly <rgiuly@gmail.com>
2017-01-05 04:59:00 +0000

    brightness

--- send_video.py
+++ send_video.py
@@ -108,10 +108,10 @@ def handleLinux(deviceNumber, videoPort):
     #print err
 
 
-    os.system("v4l2-ctl -c brightness=10 -c contrast=25 -c saturation=40") # Timmy
+    #os.system("v4l2-ctl -c brightness=10 -c contrast=25 -c saturation=40") # Timmy
     #os.system("v4l2-ctl -c brightness=10 -c contrast=25 -c saturation=40")
-    os.system("v4l2-ctl -c brightness=240 -c contrast=75 -c saturation=60") # Skippy   
-    #os.system("v4l2-ctl -c brightness=50 -c contrast=50 -c saturation=80")
+    #os.system("v4l2-ctl -c brightness=240 -c contrast=75 -c saturation=60") # Skippy   
+    os.system("v4l2-ctl -c brightness=150 -c contrast=50 -c saturation=80") # Marvin
     #os.system("v4l2-ctl -c brightness=200 -c contrast=100 -c saturation=100")
     
 
