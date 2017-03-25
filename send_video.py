2568a8b8f771258dd822ea8e7898113a0e0aad30
Theodore Lee <theo825@gmail.com>
2017-03-25 13:49:24 -0700

    call ffmpeg from path

--- send_video.py
+++ send_video.py
@@ -195,7 +195,7 @@ def handleWindows(deviceNumber, videoPort):
     device = devices[int(deviceAnswer)]
     #commandLine = 'ffmpeg -s 320x240 -f dshow -i video="%s" -f mpeg1video -b 200k -r 20 http://%s:%s/hello/320/240/' % (device, server, videoPort)
     # New ffmpeg command for new JSMpeg and including audio
-    commandLine = '/usr/local/bin/ffmpeg -f alsa -ar 44100 -ac 1 -i hw:1 -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video%s -f mpegts -codec:v mpeg1video -s 640x480 -b:v 250k -bf 0 http://%s:%s/hello/640/480/' % (deviceAnswer, server, videoPort)
+    commandLine = 'ffmpeg -f alsa -ar 44100 -ac 1 -i hw:1 -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video%s -f mpegts -codec:v mpeg1video -s 640x480 -b:v 250k -bf 0 http://%s:%s/hello/640/480/' % (deviceAnswer, server, videoPort)
     
 
     process = runFfmpeg(commandLine)
