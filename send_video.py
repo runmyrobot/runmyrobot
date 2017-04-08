7d2b92d4c8798bf75b55c9349ba00bd20552f4f5
Rick Giuly <rgiuly@gmail.com>
2017-04-07 18:59:44 -0700

    print command lines

--- send_video.py
+++ send_video.py
@@ -184,6 +184,10 @@ def handleLinux(deviceNumber, videoPort, audioPort):
     videoCommandLine = '/usr/local/bin/ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video%s -f mpegts -codec:v mpeg1video -s 640x480 -b:v %dk -bf 0 -muxdelay 0.001 http://%s:%s/hello/640/480/' % (deviceAnswer, args.kbps, server, videoPort)
     audioCommandLine = '/usr/local/bin/ffmpeg -f alsa -ar 44100 -ac 1 -i hw:1 -f mpegts -codec:a mp2 -b:a 32k -muxdelay 0.001 http://%s:%s/hello/640/480/' % (server, audioPort)
 
+
+    print videoCommandLine
+    print audioCommandLine
+    
     videoProcess = runFfmpeg(videoCommandLine)
     audioProcess = runFfmpeg(audioCommandLine)
 
