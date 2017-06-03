51f08405eed12e0cd6efefae6e543d37d57fe6e6
blddk <michael.andresen@gmail.com>
2017-06-03 18:10:25 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
+sudo apt-get install -y python-serial python-dev libgnutls28-dev espeak python-smbus python-pip git
 
 sudo pip install -U socketIO-client &&\
+
 cd ~ &&\
+
 git clone https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library.git &&\
 cd Adafruit-Motor-HAT-Python-Library &&\
+
 sudo apt-get -y install python-dev &&\
 sudo python setup.py install &&\
+
 cd ~ &&\
 wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.25.tar.bz2 &&\
 tar xjf alsa-lib-1.0.25.tar.bz2 &&\
@@ -12,12 +19,14 @@ cd alsa-lib-1.0.25 &&\
 ./configure --host=arm-unknown-linux-gnueabi &&\
 make -j4 &&\
 sudo make install &&\
+
 cd ~ &&\
 git clone git://git.videolan.org/x264 &&\
 cd x264 &&\
 ./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl &&\
 make -j4 &&\
 sudo make install &&\
+
 cd ~ &&\
 git clone https://github.com/FFmpeg/FFmpeg.git &&\
 cd FFmpeg &&\
@@ -25,3 +34,12 @@ cd FFmpeg &&\
 make -j4 &&\
 sudo make install
 
+cd ~
+sudo git clone https://github.com/runmyrobot/runmyrobot
+cd runmyrobot
+
+sudo cp ~/runmyrobot/scripts/start_robot ~/
+
+sudo echo "@reboot /bin/bash /home/pi/start_robot" >> /etc/crontab
+
+sudo nano ~/start_robot
